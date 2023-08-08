d7e7adee05527483e6f953af9ec5f357b4c64930
JC Nitro <66989221+jcnitro123@users.noreply.github.com>
2023-08-08 16:16:45 +0800

    Update train.py

--- train.py
+++ train.py
@@ -30,19 +30,19 @@ print()
 
 # Load Dataset
 train_data = object_detector.DataLoader.from_pascal_voc(
-    'wild_boar/train'
-    'wild_boar/train'
-    'wild_boar/train'
-    'wild_boar/train'
+    'wild_boar/train',
+    'wild_boar/train',
+    'wild_boar/train',
+    'wild_boar/train',
     'wild_boar/train',
     ['bird','boar','dog','human','monkey']
 )
 
 val_data = object_detector.DataLoader.from_pascal_voc(
-    'wild_boar/validate'
-    'wild_boar/validate'
-    'wild_boar/validate'
-    'wild_boar/validate'
+    'wild_boar/validate',
+    'wild_boar/validate',
+    'wild_boar/validate',
+    'wild_boar/validate',
     'wild_boar/validate',
     ['bird','boar','dog','human','monkey']
 )
