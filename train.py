48c1d2364ba034e4c97a0903da2ab3fd9c082a46
JC Nitro <66989221+jcnitro123@users.noreply.github.com>
2023-08-08 15:10:20 +0800

    Update train.py

--- train.py
+++ train.py
@@ -30,15 +30,13 @@ print()
 
 # Load Dataset
 train_data = object_detector.DataLoader.from_pascal_voc(
-    'android_figurine/train',
-    'android_figurine/train',
-    ['android', 'pig_android']
+    'train',
+    ['Eggs']
 )
 
 val_data = object_detector.DataLoader.from_pascal_voc(
-    'android_figurine/validate',
-    'android_figurine/validate',
-    ['android', 'pig_android']
+    'validate',
+    ['Eggs']
 )
 
 # Load model spec
